c7769aa611c3ac1945a5b94f3b186f0e676c91ab
Shubham Sengar <shubhamsengar2303@gmail.com>
2025-05-02 10:35:16 +0530

    some changes

--- frontend/src/components/ColorDisplay.jsx
+++ frontend/src/components/ColorDisplay.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+
+
+
+
 export default function ColorDisplay({ color }) {
+
+
   return (
     <div className="color-display">
       <div className="color-swatch" style={{ backgroundColor: color }}></div>
-      <span>{color}</span>
+      <div className="color-details">
+        <span>{color}</span>
+      </div>
     </div>
   );
 }
--- frontend/src/utils/getColorName.js
+++
@@ -1,7 +0,0 @@
-const getColorName = ({ r, g, b }) => {
-    const toHex = (c) => c.toString(16).padStart(2, '0');
-    return `#${toHex(r)}${toHex(g)}${toHex(b)}`.toUpperCase();
-  };
-  
-  export default getColorName;
-  
